7ebcd0564aea948bb9f15ecca5e6c0ac0ec506b6
Sumit <sumitsinghindia5@gmail.com>
2022-06-22 17:41:52 +0530

    jsk tools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 const tasks=['sumit is good boy','you are creative','this is good for health'];
-const element=React.createElement('ol',null,tasks.map((task,index)=>React.createElement('li',{key:index},task)));
+const element=<div>
+  <h1>Task List</h1>
+   <ol>
+        {tasks.map((task,index)=><li key={index}>{task}</li>)}
+        <li>hello sumit kumar </li>
+        <li>{tasks[0]}</li>
+        <li>{tasks[1]}</li>
+        <li>{tasks[2]}</li>
+</ol>
+</div>
 ReactDOM.render(element,document.getElementById('root'));
