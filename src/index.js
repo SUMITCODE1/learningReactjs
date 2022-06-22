b7580b6fda08da77c7b715599dd439852d3d4f4d
Sumit <sumitsinghindia5@gmail.com>
2022-06-22 16:53:16 +0530

    created by sumit kumar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import  ReactDOM  from 'react-dom';
+const element=React.createElement('ol',null,
+React.createElement('li',null,'sumit is good boy'),
+React.createElement('li',null,'this is good for health'),
+React.createElement('li',null,'you are creative boy'));
+ReactDOM.render(element,document.getElementById('root'));
