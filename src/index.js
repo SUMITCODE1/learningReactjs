dd58c4d52e406d4cba9d2d08e449b6104019e7e2
Sumit <sumitsinghindia5@gmail.com>
2022-06-22 17:00:11 +0530

    some change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-const element=React.createElement('ol',null,
-React.createElement('li',null,'sumit is good boy'),
-React.createElement('li',null,'this is good for health'),
-React.createElement('li',null,'you are creative boy'));
+const tasks=['sumit is good boy','you are creative','this is good for health'];
+const element=React.createElement('ol',null,tasks.map((task)=>React.createElement('li',null,task)));
 ReactDOM.render(element,document.getElementById('root'));
