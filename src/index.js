fc29557747851abde0f3ec263c6174093203724a
Sumit <sumitsinghindia5@gmail.com>
2022-06-22 17:18:45 +0530

    very basic change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 const tasks=['sumit is good boy','you are creative','this is good for health'];
-const element=React.createElement('ol',null,tasks.map((task)=>React.createElement('li',null,task)));
+const element=React.createElement('ol',null,tasks.map((task,index)=>React.createElement('li',{key:index},task)));
 ReactDOM.render(element,document.getElementById('root'));
