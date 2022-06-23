183a4ac9e08c7064d6fe7a6cf57f190b844828dd
Sumit <sumitsinghindia5@gmail.com>
2022-06-23 11:43:45 +0530

    react js App setup photowall

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,34 @@
 import React ,{Component} from "react";
-import List from './List';
+
 import Title from './Title';
+import PhotoWall from "./PhotoWall";
+
+const posts=[{
+  id:"0",
+  discription:"beautyful landscape",
+  imageLink:"Image/image1.jpg"
+
+},{
+  id:"1",
+  discription:"Alien ??",
+  imageLink:"Image/image2.jpg"
+
+},
+{
+  id:"2",
+  discription:"On a vaction",
+  imageLink:"Image/image3.jpg"
+}
+]
+
 
 
 
 class Main extends Component{
     render(){
       return(
-        <div><Title title={'toDo'}/>
-        <List tasks={['Aniket is my guru ji','He is good empo in mindtree']}/>
+        <div><Title title={'Photowall'}/>
+       <PhotoWall posts={posts}/>
               
                </div>
       )
