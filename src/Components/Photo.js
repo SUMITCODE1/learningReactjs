0ac6996ed081ad2549e3a390820a17f1c8496f52
Sumit <sumitsinghindia5@gmail.com>
2022-06-23 21:32:33 +0530

    change mistake in proptype

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -15,7 +15,7 @@ function Photo(props){
                        
     }
     
-    Photo.PropTypes={
+    Photo.propTypes ={
         post:PropTypes.object.isRequired,
         onRemovePhoto:PropTypes.func.isRequired
     }
