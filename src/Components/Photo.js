25c33358116d6f81fa26d53eca999a9e3a4ffc6f
Sumit <sumitsinghindia5@gmail.com>
2022-06-23 16:51:36 +0530

    some style in app

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -4,8 +4,13 @@ class Photo extends Component{
     render(){
         const post=this.props.post;
         return <figure class="figure"> 
-            <img src={post.imageLink} alt="image"/>
+            <img className="photo" src={post.imageLink} alt={post.discription}/>
+            <figcaption><p>{post.discription}</p></figcaption> 
+            <div className="button-container">
+            <button className="remove-button">Remove</button>
+            </div>
                         </figure>
+                       
     }
 }
 export default Photo;
