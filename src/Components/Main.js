660bb90d0b408b96ade9c915f8b8bb4ccdcd9bcf
Sumit <sumitsinghindia5@gmail.com>
2022-06-24 23:41:30 +0530

    component state Navigation

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,8 @@ import React ,{Component} from "react";
 import Title from './Title';
 import PhotoWall from "./PhotoWall";
 import { Simulate } from "react-dom/test-utils";
+import AddPhoto from "./AddPhoto";
+
 
 
 
@@ -13,9 +15,26 @@ class Main extends Component{
   constructor(){
     super();
     this.state ={
-      posts:[]
+      posts:[{
+        id:"0",
+        discription:"Cat",
+        imageLink:"Image/image1.jpg"
+      
+      },{
+        id:"1",
+        discription:"Laptop with notebook",
+        imageLink:"Image/image2.jpg"
+      
+      },
+      {
+        id:"2",
+        discription:"Laptop",
+        imageLink:"Image/image3.jpg"
+      }],
+      screen:'photos'
   }
   this.removePhoto=this.removePhoto.bind(this);
+  this.navigate=this.navigate.bind(this);
   }
     removePhoto(postRemoved){
       console.log(postRemoved.discription);
@@ -24,39 +43,40 @@ class Main extends Component{
       })
       )
 
+
     }
-    componentDidMount(){
-      const data=SimulateFetchFromDatabase();
+    navigate(){
       this.setState({
-        posts:data
+        screen:'addPhoto'
       })
+    }
+    componentDidUpdate(prevProps,prevState){
+      console.log(prevState.posts);
+      console.log(this.state);
     
     }
     render(){
       return(
-        <div><Title title={'Photowall'}/>
-       <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto}/>
-              
+        <div>
+          {
+            this.state.screen==='photos' &&(
+          <div>
+          <Title title={'Photowall'}/>
+          <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/>
+          </div>
+            )
+    }
+    {
+        this.state.screen ==='addPhoto' &&(
+        <div>
+            <AddPhoto/>
+        </div>  
+        )
+      
+    }
                </div>
       )
     }
   }
-  function SimulateFetchFromDatabase(){
-    return [{
-      id:"0",
-      discription:"Cat",
-      imageLink:"Image/image1.jpg"
-    
-    },{
-      id:"1",
-      discription:"Laptop with notebook",
-      imageLink:"Image/image2.jpg"
-    
-    },
-    {
-      id:"2",
-      discription:"Laptop",
-      imageLink:"Image/image3.jpg"
-    }]
-  }
+  
   export default Main;
